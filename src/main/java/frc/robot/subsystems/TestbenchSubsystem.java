33b3d14194557b507fa8799a2313728ee79842e1
Daniel Blittschau <dblitt99@gmail.com>
2019-10-23 16:42:31 -0500

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/TestbenchSubsystem.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.TestbenchCommand;
+
+/**
+ * Add your docs here.
+ */
+public class TestbenchSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  // motor controllers
+  public WPI_TalonSRX talonsrx0 = new WPI_TalonSRX(RobotMap.TALONSRX0_CAN);
+  
+  // button board
+  public DigitalInput talonSwitch = new DigitalInput(RobotMap.TALONSRX0_DIO);
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new TestbenchCommand());
+  }
+}
